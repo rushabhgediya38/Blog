b43d642e7a221a337db42d2d832f6ed1ef3d2571
rushabh Gediya <rushabhgediya38@gmail.com>
2020-08-11 23:45:26 -0700

    Create Blog Api

--- coryshafer/urls.py
+++ coryshafer/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('register/', user_views.register, name='register'),
     path('', include('blog.urls')),
+    path('api/', include('api.urls')),
 ]
 
 if settings.DEBUG:
